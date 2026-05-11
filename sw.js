f14dd558d89d2c333a46110d6184e1c906bc9869
Heath Chartier <heathchartier@github.com>
2026-05-10 21:37:56 -0700

    Add PIN lock, Face ID, order log, call outcomes, birthday alerts, fix calendar overlay

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hcroutes-v3';
+const CACHE = 'hcroutes-v4';
 const ASSETS = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/icon.svg'];
 
 self.addEventListener('install', e => {
